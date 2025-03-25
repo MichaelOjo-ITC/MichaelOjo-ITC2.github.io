77a41e2da8a699747c09b2383ac8c0bdf241eca7
Michael Ojo <m09ojo@siena.edu>
2025-03-25 08:17:18 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("sienaForm").addEventListener("submit", function (e) {
+    e.preventDefault(); // Stop the form from submitting automatically
+    validateForm(); // Call the validation function
+});
